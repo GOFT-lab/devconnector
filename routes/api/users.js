d2a6a407923dada245c00ccc75b461f6963b5b31
Svyatoslav Suprun <sviatoslav.suprun@nure.ua>
2023-08-25 12:20:15 +0300

    user authorization

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,9 +4,9 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
+const config = require("config");
 
 const User = require("../../models/User");
-const config = require("config");
 
 // @route POST api/users
 // @desc Register user
